e76c2dad963ef9181056f84c8565de71e483503a
Lawrence Taylor <ltaylor0430@yahoo.com>
2012-10-20 21:20:35 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   before_save { |user| user.email =  email.downcase}
-
+  before_save :create_remember_token
   validates :name,  presence: true, 
   					length: { maximum: 50 }
  
@@ -24,4 +24,9 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6}
   validates :password_confirmation, presence: true
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
